d484851ec8a6fee3d2dca5801578f71211c6c5bd
coursecomputer <coursecomputer@outlook.com>
2019-10-27 18:09:14 +0100

    test: Add setup test file and add table test file

---
+++ test/table_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/AlgoCafe/boyer-moore-horspool/bmh"
+)
+
+// Additional function
+func initContent(num int) (content []int) {
+	content = make([]int, 256)
+
+	for i := 0; i < 256; i++ {
+		content[i] = num
+	}
+
+	return content
+}
+
+
+var _ = Describe("Table", func() {
+	var table = bmh.Table{}
+
+	Context("Success", func() {
+		It("with \"tuvzzzzzzzx\" as argument", func() {
+			// Prepare
+			var content []int
+			var pattern = []byte("tuvzzzzzzzx")
+
+			content = initContent(len(pattern))
+
+			content[int('t')] = 10
+			content[int('u')] = 9
+			content[int('v')] = 8
+			content[int('z')] = 1
+
+			// Process
+			table.Build(pattern)
+
+			// Expect
+			Expect(table.Content).To(Equal(content))
+		})
+	})
+})
